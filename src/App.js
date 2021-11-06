d64cbacbb6c6f155c5f16fa83efcdab5406da758
Vali <toeavalidaniel@gmail.com>
2021-11-06 15:46:23 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./App.scss";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 import HomePage from "./pages/HomePage/HomePage";
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<HomePage />} />
-      </Routes>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<HomePage />} />
+        </Routes>
+      </Router>
+    </Provider>
   );
 };
 
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { changePlatform } from "../../redux/platformSlice";
 
 const HomePage = () => {
-    return (
-        <div>
-            home
-        </div>
-    )
-}
+  const selectedPlatform = useSelector((state) => state.platform.value);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <select
+        onChange={(e) => dispatch(changePlatform(e.target.value))}
+        value={selectedPlatform}
+      >
+        <option value="Facebook">Facebook</option>
+        <option value="Instagram">Instagram</option>
+        <option value="Twitter">Twitter</option>
+      </select>
+    </div>
+  );
+};
 
 export default HomePage;
-

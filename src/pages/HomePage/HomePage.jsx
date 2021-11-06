b43b3e78f07e7465dd9c27cfb2bd32b743f280dc
Sergiu Voicu <sergiu.ciprian.voicu@gmail.com>
2021-11-06 15:21:34 +0200

    layout changes

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,44 @@
 import React from 'react'
-
+import { AppShell, Navbar, Header, ThemeIcon, Title, Select, Autocomplete } from '@mantine/core';
+import './HomePage.scss'
 const HomePage = () => {
     return (
-        <div>
-            home
-        </div>
+        <AppShell
+            padding="md"
+            navbar={
+                <Navbar height={600} padding="xs">
+                    <Navbar.Section className="title">
+                        <ThemeIcon></ThemeIcon>
+                        <Title order={3}>Application</Title>
+                    </Navbar.Section>
+                    <Navbar.Section mt="lg" className="unstyledButtonSelected"><span className="unstyledButtonTextSelected">Something</span></Navbar.Section>
+
+                    <Navbar.Section mt="lg" className="unstyledButton"><span className="unstyledButtonText">SomethingElse</span></Navbar.Section>
+                    <Navbar.Section mt="lg" className="unstyledButton"><span className="unstyledButtonText">Anything</span></Navbar.Section>
+                </Navbar>
+            }
+        >
+            <Header className="header">
+                <div className="header-container">
+                    <Title order={3}>Yessir </Title>
+                    <div className="header-cell">
+                        <Select
+                            placeholder="Pick one"
+                            data={[{ value: 'react', label: 'React' },
+                            { value: 'ng', label: 'Angular' },
+                            { value: 'svelte', label: 'Svelte' },
+                            { value: 'vue', label: 'Vue' },]}></Select>
+
+                        <Autocomplete
+                            placeholder="Search by tag"
+                            data={['React', 'Angular', 'Svelte', 'Vue']}
+                        />
+                    </div>
+                </div>
+            </Header>
+        </AppShell>
+
+
     )
 }
 
